de0e76d6fe2e0838a0a50a8de27c9fb2da6ea81e
sachin jakhar <mesachinjakhar@gmail.com>
2024-09-18 03:39:39 +0530

    first commit

---
+++ week0/day1/index.js
@@ -0,0 +1,20 @@
+    // creating 2 h1 elements
+    const heading1 = React.createElement("h1", {
+        id: "title",
+    }, "Heading 1" )
+
+    const heading2 = React.createElement("h1", {
+        id: "title",
+    }, "Heading 2" )
+
+    // creating div to store created h1 elements
+    const div = React.createElement("div", {
+        id: "container"
+    }, [heading1, heading2])
+
+
+    // create root
+    const root = ReactDOM.createRoot(document.getElementById("root"))
+
+    // insert div into root
+    root.render(div)
